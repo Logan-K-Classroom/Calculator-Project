daf5afafc3e2c640380d0000701ade9efd2344e9
LogEman <thelogankessler@gmail.com>
2024-09-30 16:58:11 +0000

    All functions to be made for calculator

---
+++ Division.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        //Gets and stores inputs from the autograder. 
+        Scanner graderInput = new Scanner(System.in);
+
+        double firstNumber = graderInput.nextDouble();
+        double secondNumber = graderInput.nextDouble();
+
+
+        //Default output for result
+        double result = 0;
+
+        System.out.print(result);
+    }
+}
